8d505d8d10b64de24d2a614d7f687cddde1a11b2
Junghoon_Suk <sukjh87@gmail.com>
2020-10-24 17:29:02 +0800

    Express : PATCH & DELETE REQUEST + refactoring with utils.js & app.route()

--- express-project/code-along/app.js
+++ express-project/code-along/app.js
@@ -1,75 +1,23 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
 const app = express();
 
-// Read a local json file sync
-const pathTourSimple = path.join(
-    __dirname,
-    'dev-data',
-    'data',
-    'tours-simple.json'
-);
+// Local Module from from utli.js
+const {
+    getAllTours,
+    getSingleTour,
+    addTour,
+    updateTour,
+    deleteTour,
+} = require('./utils');
 
-const tours = JSON.parse(fs.readFileSync(pathTourSimple));
-
-// express.json() is a middleware
-// that stays between request and response
-// that can modify incoming request data
 app.use(express.json());
 
-// GET request
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            // ES6 not need specifying
-            // both key : value if same name
-            tours,
-        },
-    });
-});
+app.route('/api/v1/tours').get(getAllTours).post(addTour);
 
-// GET Request - URL parameters i.e. individual data
-// /:x /:y?(optional)
-app.get('/api/v1/tours/:id', (req, res) => {
-    // console.log(req.params);
-
-    const id = req.params.id * 1;
-    const tour = tours.find((el) => el.id === id);
-
-    // if (id < tours.length) {}
-    if (tour) {
-        res.status(200).json({
-            status: 'success',
-            data: {
-                tour,
-            },
-        });
-    } else {
-        res.status(404).json({ message: `No page found with id ${id}` });
-    }
-});
-
-// POST request
-app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body);
-
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    tours.push(newTour);
-
-    fs.writeFile(pathTourSimple, JSON.stringify(tours), (err) => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour,
-            },
-        });
-    });
-});
+app.route('/api/v1/tours/:id')
+    .get(getSingleTour)
+    .patch(updateTour)
+    .delete(deleteTour);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on PORT ${PORT}`));
---
+++ express-project/code-along/utils.js
@@ -0,0 +1,134 @@
+// LOAD MODULES
+const fs = require('fs');
+const path = require('path');
+
+// LOAD LOCAL DATA
+const tours = require('./dev-data/data/tours-simple.json');
+const dataFilePath = path.join(
+    __dirname,
+    'dev-data',
+    'data',
+    'tours-simple.json'
+);
+
+// HELPER FUNCTION
+function writeDataToFile(filePath, data) {
+    fs.writeFileSync(filePath, JSON.stringify(data), 'utf-8', (err) => {
+        console.log(err);
+    });
+}
+
+function update(id, newData) {
+    const index = tours.findIndex((tour) => tour.id === id);
+    tours[index] = { id, ...newData };
+    writeDataToFile(dataFilePath, tours);
+    return tours[index];
+}
+
+function remove(id) {
+    const updatedData = tours.filter((tour) => tour.id !== id);
+    writeDataToFile(dataFilePath, updatedData);
+}
+
+// HTTP REQUESTS
+
+// @ GET /api/v1/tours
+
+function getAllTours(req, res) {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours,
+        },
+    });
+}
+
+// @ GET /api/v1/tours/:id
+
+function getSingleTour(req, res) {
+    const id = req.params.id * 1;
+    const tour = tours.find((el) => el.id === id);
+
+    // if (id < tours.length) {}
+    if (tour) {
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour,
+            },
+        });
+    } else {
+        res.status(404).json({ message: `No page found with id ${id}` });
+    }
+}
+
+// @ POST /api/v1/tours
+
+function addTour(req, res) {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+
+    writeDataToFile(dataFilePath, tours);
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour,
+        },
+    });
+}
+
+function updateTour(req, res) {
+    const reqId = req.params.id * 1;
+    const reqTour = tours.find((tour) => tour.id === reqId);
+
+    if (reqTour) {
+        const reqBody = req.body;
+        const { name, duration, maxGroupSize, difficulty } = reqBody;
+
+        const newData = {
+            name: name || reqTour.name,
+            duration: duration || reqTour.duration,
+            maxGroupSize: maxGroupSize || reqTour.maxGroupSize,
+            difficulty: difficulty || reqTour.dificulty,
+        };
+
+        const updatedProduct = update(reqId, newData);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                updatedProduct,
+            },
+        });
+    } else {
+        res.status(404).json({
+            msg: 'Page not found',
+        });
+    }
+}
+
+function deleteTour(req, res) {
+    const delId = req.params.id * 1;
+    const delTour = tours.find((tour) => tour.id === delId);
+
+    if (delTour) {
+        remove(delId);
+
+        res.status(204).json({
+            msg: 'Success',
+        });
+    } else {
+        res.status(404).json({
+            msg: 'Page not found',
+        });
+    }
+}
+
+module.exports = {
+    getAllTours,
+    getSingleTour,
+    addTour,
+    updateTour,
+    deleteTour,
+};
