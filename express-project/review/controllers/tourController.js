f5a74e2014e9db252c078101095fbc2226a40d82
Junghoon_Suk <sukjh87@gmail.com>
2020-11-11 16:24:21 +0800

    Review : Request query & aggregate pipeline

--- express-project/review/controllers/tourController.js
+++ express-project/review/controllers/tourController.js
@@ -2,7 +2,42 @@ const Tour = require('../models/tourModel');
 
 async function getAllTours(req, res) {
   try {
-    const allTours = await Tour.find();
+    // 1) FILTERING
+    let queryObj = { ...req.query };
+
+    const queryToExclude = ['sort', 'fields', 'page', 'limit'];
+    queryToExclude.forEach((key) => delete queryObj[key]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(lt|lte|gt|gte)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) SORT
+    if (req.query.sort) {
+      const reqQuery = req.query.sort.split(',').join(' ');
+      query = query.sort(reqQuery);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) LIMIT FIELDS
+    if (req.query.fields) {
+      const reqQuery = req.query.fields.split(',').join(' ');
+      query = query.select(reqQuery);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) PAIGINATION
+    const reqPage = req.query.page * 1 || 1;
+    const reqLimit = req.query.limit * 1 || 100;
+    const skip = (reqPage - 1) * reqLimit;
+
+    query = query.skip(skip).limit(reqLimit);
+
+    // EXECUTE QUERY
+    const allTours = await query;
 
     res.status(200).json({
       status: 'success',
@@ -92,10 +127,90 @@ async function deleteTour(req, res) {
   }
 }
 
+// AGGREGATE PIPELINE
+async function getTourStats(req, res) {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: null,
+          countTours: { $sum: 1 },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgDuration: { $avg: '$duration' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+}
+
+async function getMonthlyTourStats(req, res) {
+  try {
+    const reqYear = req.params.year * 1;
+    const monthlyStats = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${reqYear}-01-01`),
+            $lte: new Date(`${reqYear}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTours: { $sum: 1 },
+          tourNames: { $push: '$name' },
+        },
+      },
+      {
+        $sort: { _id: 1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        monthlyStats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+}
+
 module.exports = {
   getAllTours,
   getTour,
   addTour,
   updateTour,
   deleteTour,
+  getTourStats,
+  getMonthlyTourStats,
 };
--- express-project/review/routers/tourRouter.js
+++ express-project/review/routers/tourRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { route } = require('../app');
 const router = express.Router();
 
 const {
@@ -7,7 +8,12 @@ const {
   addTour,
   updateTour,
   deleteTour,
+  getTourStats,
+  getMonthlyTourStats,
 } = require('../controllers/tourController');
+
+router.route('/get-stats').get(getTourStats);
+router.route('/get-monthly-stats/:year').get(getMonthlyTourStats);
 
 router.route('/').get(getAllTours).post(addTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
