0b7a8240a4998ff5160055146a37fc9fcd74ab33
Junghoon_Suk <sukjh87@gmail.com>
2020-11-25 12:51:35 +0800

    Connect to MongoDB & handle errors + color console log

--- devcamper-app/coursework/server.js
+++ devcamper-app/coursework/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+const dotenv = require('dotenv');   // process.env - environment variable 
+const morgan = require('morgan');   // logger middleware
+const colors = require('colors');     // style console log (https://github.com/marak/colors.js/)
 
-// Load routes
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
+
+// Connect to Database
+connectDB();
+
+// Load routes
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -16,8 +22,15 @@ app.use(morgan('dev')); // function with 'dev' parameter
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5500;
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.yellow.bold
     )
 );
+
+// Handle database promise error : UnhandledPromiseRejectionWarning
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red.bold);
+    // Close server & exit process with fail
+    server.close(() => process.exit(1));        
+})
